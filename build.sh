1ba766cd9b96dd8828cd0867bc05ea9645268135
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2022-08-05 20:27:34 +0200

    Integrate CSS into a single HTML file

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-pandoc --css=styling.css -s -f markdown+smart --toc --metadata \
-pagetitle="Quill Simple Guide" --to=html5 quill-guide.md \
+cp quill-guide.md tmp.md
+echo "<style>" >> tmp.md && cat styling.css >> tmp.md && echo "</style>" >> tmp.md
+pandoc -s -f markdown+smart --toc --metadata \
+pagetitle="Quill Simple Guide" --to=html5 tmp.md \
 -o quill-guide.html && \
 xdg-open quill-guide.html
+rm tmp.md
